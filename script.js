846e82b7c995c4777b464ad69379f31c60846591
Anton Peter <antonpeterp@gmail.com>
2025-12-12 14:43:59 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,19 @@
+setInterval(render, 1000);
+setInterval(render, 1000);
+
+function render() {
+  const now = new Date();
+  const newYear = new Date("Jan 1, 2026 00:00:00");
+
+  const diff = newYear - now;
+
+  const days = Math.floor(diff / (1000 * 60 * 60 * 24));
+  const hours = Math.floor((diff / (1000 * 60 * 60)) % 24);
+  const minutes = Math.floor((diff / (1000 * 60)) % 60);
+  const seconds = Math.floor((diff / 1000) % 60);
+
+  document.getElementById("days").innerText = days;
+  document.getElementById("hours").innerText = hours;
+  document.getElementById("minutes").innerText = minutes;
+  document.getElementById("seconds").innerText = seconds;
+}
